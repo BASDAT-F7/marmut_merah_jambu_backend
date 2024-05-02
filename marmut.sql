546e5f971a41c170a5eb5fd860e9a0d476805298
AtharAdista <117746860+AtharAdista@users.noreply.github.com>
2024-05-02 18:45:46 +0700

    Fix : forget comma in GENRE table

--- marmut.sql
+++ marmut.sql
@@ -8,7 +8,7 @@ CREATE TABLE KONTEN(
 
 CREATE TABLE GENRE(
     id_konten UUID,
-    genre VARCHAR(50)
+    genre VARCHAR(50),
     PRIMARY KEY(id_konten, genre),
     FOREIGN KEY(id_konten) REFERENCES KONTEN (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
